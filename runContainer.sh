167fafd510493907ee2b5d507977828325219993
jefferson-norberto2 <jmn@cin.ufpe.br>
2025-03-16 09:34:26 -0300

    FIX: Numpy int64 convert

--- runContainer.sh
+++ runContainer.sh
@@ -1,7 +1,7 @@
 WORKDIR=/app/
 
-docker run --gpus all --rm -it \
---mount type=bind,src=./,dst=${WORKDIR}/DynamicDet/ \
---mount type=bind,src=${HOME}/dev/Datasets/,dst=${WORKDIR}/DynamicDet/dataset/ \
---detach \
-dynamicdet:latest 
+docker run --gpus all -it -d \
+--mount type=bind,src=./,dst=${WORKDIR}/DynamicDetEx/ \
+--mount type=bind,src=${HOME}/dev/Datasets/,dst=${WORKDIR}/DynamicDetEx/dataset/ \
+--name dynamic_det \
+dynamicdet:latest bash
